17f9ad3efa09d425ae727fa3fbc26b67831158f8
pnrmct <pinarmacit@Pnar-MacBook-Air.local>
2022-05-16 17:58:55 +0300

    first commit

---
+++ RickandMortyMvvm/Feutures/RickMorty/View/RickMortyViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RickMortyViewController.swift
+//  RickandMortyMvvm
+//
+//  Created by Pınar Macit on 14.05.2022.
+//
+
+import UIKit
+import SnapKit
+
+final class RickMortyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
